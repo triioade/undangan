01938586d55243d29c99fd1e9f7c3cf80dd027b9
Triioade <trioade29@gmail.com>
2025-06-18 18:26:46 +0700

    Initial commit for wedding invitation

---
+++ src/config/config.js
@@ -0,0 +1,58 @@
+const config = {
+  data: {
+    title: "Undangan Pernikahan",
+    description:
+      "Kami akan menikah dan mengundang Anda untuk turut merayakan momen istimewa ini.",
+    groomName: "Trio Ade Pamungkas",
+    brideName: "Calon",
+    parentGroom: "Bapak  & Ibu ",
+    parentBride: "Bapak  & Ibu ",
+    date: "2025-06-19",
+    maps_url: "https://goo.gl/maps/abcdef",
+    maps_embed:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0000000000005!2d106.8270733147699!3d-6.175392995514422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4f1b6d7b1e7%3A0x2e69f4f1b6d7b1e7!2sMonumen%20Nasional!5e0!3m2!1sid!2sid!4v1633666820004!5m2!1sid!2sid",
+    time: "10:00 - 17:30 WIB",
+    location: "Dirumah saya",
+    address: "Di Bogor, Jawa Barat",
+    ogImage: "/images/og-image.jpg",
+    favicon: "/images/favicon.ico",
+    agenda: [
+      {
+        title: "Akad Nikah",
+        date: "2025-06-20",
+        startTime: "10:00",
+        endTime: "17:30",
+        location: "Dirumah saya",
+        address: "Disini",
+      },
+      {
+        title: "Resepsi Nikah",
+        date: "2025-06-20",
+        startTime: "10:00",
+        endTime: "17:30",
+        location: "Dirumah saya",
+        address: "Disini",
+      },
+    ],
+    audio: {
+      src: "/audio/rapsodi.mp3",
+      title: "Rapsodi",
+      autoplay: true,
+      loop: true,
+    },
+    banks: [
+      {
+        bank: "Bank Central Asia",
+        accountNumber: "1672183159",
+        accountName: "Trio Ade Pamungkas", 
+      },
+      {
+        bank: "Bank Bri",
+        accountNumber: "082101052608537",
+        accountName: "Trio Ade Pamungkas",
+      },
+    ],
+  },
+};
+
+export default config;
