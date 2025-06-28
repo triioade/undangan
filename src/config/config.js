e3dd9ec1bb15df25876ec22f8ebb86b4d8ad1f74
Triioade <trioade29@gmail.com>
2025-06-28 19:39:19 +0700

    a

--- src/config/config.js
+++ src/config/config.js
@@ -7,7 +7,7 @@ const config = {
     brideName: "Calon",
     parentGroom: "Bapak  & Ibu ",
     parentBride: "Bapak  & Ibu ",
-    date: "2025-06-19",
+    date: "2025-07-19",
     maps_url: "https://goo.gl/maps/abcdef",
     maps_embed:
       "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.0000000000005!2d106.8270733147699!3d-6.175392995514422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f4f1b6d7b1e7%3A0x2e69f4f1b6d7b1e7!2sMonumen%20Nasional!5e0!3m2!1sid!2sid!4v1633666820004!5m2!1sid!2sid",
@@ -19,7 +19,7 @@ const config = {
     agenda: [
       {
         title: "Akad Nikah",
-        date: "2025-06-20",
+        date: "2025-07-20",
         startTime: "10:00",
         endTime: "17:30",
         location: "Dirumah saya",
@@ -27,7 +27,7 @@ const config = {
       },
       {
         title: "Resepsi Nikah",
-        date: "2025-06-20",
+        date: "2025-07-20",
         startTime: "10:00",
         endTime: "17:30",
         location: "Dirumah saya",
